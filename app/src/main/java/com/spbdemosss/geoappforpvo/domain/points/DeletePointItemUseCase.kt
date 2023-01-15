9941ed975197b2b824a116abfa562636d23dca10
demosss <spbdemosss@gmail.com>
2023-01-15 19:45:09 +0300

    les3

--- app/src/main/java/com/spbdemosss/geoappforpvo/domain/points/DeletePointItemUseCase.kt
+++ app/src/main/java/com/spbdemosss/geoappforpvo/domain/points/DeletePointItemUseCase.kt
@@ -2,6 +2,6 @@ package com.spbdemosss.geoappforpvo.domain.points
 
 class DeletePointItemUseCase(private val pointListRepository: PointListRepository) {
     fun deletePointItem(pointItem: PointItem){
-        pointListRepository.addPointItemUseCase(pointItem)
+        pointListRepository.deletePointItem(pointItem)
     }
 }
