8f80ffb898f3645282e9d6f19417bac01aa2a857
demosss <spbdemosss@gmail.com>
2023-01-15 21:00:19 +0300

    les4

---
+++ app/src/main/java/com/spbdemosss/geoappforpvo/presentation/points/ActivityPoints.kt
@@ -0,0 +1,20 @@
+package com.spbdemosss.geoappforpvo.presentation.points
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.spbdemosss.geoappforpvo.R
+
+class ActivityPoints : AppCompatActivity() {
+    private lateinit var viewModel: MainPointViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_points)
+        viewModel = ViewModelProvider(this)[MainPointViewModel::class.java]
+        viewModel.pointList.observe(this){
+            Log.d("MainAct", it.toString())
+        }
+        viewModel.getPointList()
+    }
+}
---
+++ app/src/main/java/com/spbdemosss/geoappforpvo/presentation/points/MainPointViewModel.kt
@@ -0,0 +1,29 @@
+package com.spbdemosss.geoappforpvo.presentation.points
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.spbdemosss.geoappforpvo.data.PointListRepositoryImpl
+import com.spbdemosss.geoappforpvo.domain.points.DeletePointItemUseCase
+import com.spbdemosss.geoappforpvo.domain.points.EditPointItemUseCase
+import com.spbdemosss.geoappforpvo.domain.points.GetPointListUseCase
+import com.spbdemosss.geoappforpvo.domain.points.PointItem
+
+class MainPointViewModel: ViewModel() {
+    private val repository = PointListRepositoryImpl
+
+    private val getPointListUseCase = GetPointListUseCase(repository)
+    private val deletePointItemUseCase = DeletePointItemUseCase(repository)
+    private val editPointItemUseCase = EditPointItemUseCase(repository)
+
+    val pointList = MutableLiveData<List<PointItem>>()
+
+    fun getPointList(){
+        val list = getPointListUseCase.getPointList()
+        pointList.value = list
+    }
+
+    fun deletePointItem(pointItem: PointItem){
+        deletePointItemUseCase.deletePointItem(pointItem)
+        getPointList()
+    }
+}
